87ab568380cef092f6ad660269561e87dc295d70
Nicole Williams <nickawilliams99@gmail.com>
2022-12-17 11:08:50 +0000

    C program prints last digit of number stored in n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main: Entry point
+ * main - Entry point
  * Description: Print last digit of number in assigned variable
  * Return: Always 0 (success)
  */
