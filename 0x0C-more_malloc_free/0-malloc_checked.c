0d3c84600b9e325b752f6bc3df8fe0d799617fa5
Nicole Williams <nickawilliams99@gmail.com>
2023-03-27 13:20:54 +0000

    C function allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - aloocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+		exit(98);
+	return (mem);
+}
